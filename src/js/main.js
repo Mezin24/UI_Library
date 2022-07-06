8b88d539416451a7a99b7cc76832f82419f803a9
Pavel Mezencev <mezencev24@mail.ru>
2022-07-06 12:49:07 +0300

    add fadeToggle effect module

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,9 @@
 import $ from './lib/lib';
 
-$('#first').click(() => $('.container div').eq(0).fadeOut(2000));
-$('[data-count="second"]').click(() => $('.container div').eq(1).fadeOut(2000));
+$('#first').click(() => $('.container div').eq(0).fadeToggle(2000));
+$('[data-count="second"]').click(() =>
+  $('.container div').eq(1).fadeToggle(2000)
+);
 $('button')
   .eq(2)
-  .click(() => $('.container div').eq().fadeIn(2000));
+  .click(() => $('.container div').eq().fadeToggle(2000));
