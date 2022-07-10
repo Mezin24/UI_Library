1685e232d0d2723ae52298e989652ea6d070fae1
Pavel Mezencev <mezencev24@mail.ru>
2022-07-10 17:35:18 +0300

    add js create modal functionality

--- src/js/lib/components/modal.js
+++ src/js/lib/components/modal.js
@@ -1,6 +1,6 @@
 import $ from '../core';
 
-$.prototype.modal = function () {
+$.prototype.modal = function (created) {
   for (let i = 0; i < this.length; i++) {
     const target = this[i].getAttribute('data-target');
     $(this[i]).click((e) => {
@@ -20,6 +20,10 @@ $.prototype.modal = function () {
       $(target).fadeOut(500);
       document.documentElement.style.marginRight = `0px`;
       document.documentElement.style.overflow = 'auto';
+
+      if (created) {
+        document.querySelector(target).remove();
+      }
     }
 
     function calcScrollWidth() {
@@ -45,6 +49,55 @@ $.prototype.modal = function () {
         closeModal();
       }
     });
+  }
+};
+
+$.prototype.createModal = function ({
+  text: { title, body },
+  btns: { settings, count },
+}) {
+  for (let i = 0; i < this.length; i++) {
+    const modal = document.createElement('div');
+    modal.classList.add('modal');
+    modal.setAttribute('id', this[i].getAttribute('data-target').slice(1));
+    const buttons = [];
+
+    for (let j = 0; j < count; j++) {
+      const button = document.createElement('button');
+      button.classList.add('btn', ...settings[j][1]);
+      button.textContent = settings[j][0];
+      if (settings[j][2]) {
+        button.setAttribute('data-close', true);
+      }
+
+      if (settings[j][3] && typeof settings[j][3] === 'function') {
+        button.addEventListener('click', settings[j][3]);
+      }
+
+      buttons.push(button);
+    }
+
+    modal.innerHTML = `
+      <div class="modal-dialog">
+        <div class="modal-content">
+          <button class="close" data-close>
+            <span>&times;</span>
+          </button>
+          <div class="modal-header">
+            <div class="modal-title">${title}</div>
+          </div>
+          <div class="modal-body">${body}</div>
+          <div class="modal-footer"></div>
+        </div>
+      </div>
+    
+    `;
+
+    modal.querySelector('.modal-footer').append(...buttons);
+
+    $(modal).fadeIn(500);
+    document.documentElement.append(modal);
+    $(this[i]).modal(true);
   }
 };
 
--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,17 @@
 import $ from './lib/lib';
+
+$('#trigger').click(function (e) {
+  $(this).createModal({
+    text: {
+      title: 'Modal title #2',
+      body: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Repudiandae mollitia,',
+    },
+    btns: {
+      count: 2,
+      settings: [
+        ['Close', ['btn-danger'], true],
+        ['Submit', ['btn-success', 'ml-20'], false, () => alert('submit')],
+      ],
+    },
+  });
+});
