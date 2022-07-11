c039f3075818e91a2997d989204ed770e8425daa
Pavel Mezencev <mezencev24@mail.ru>
2022-07-11 14:55:51 +0300

    add slider and build script

--- src/js/lib/components/modal.js
+++ src/js/lib/components/modal.js
@@ -102,3 +102,20 @@ $.prototype.createModal = function ({
 };
 
 $('[data-toggle="modal"]').modal();
+
+//////////
+// $('#trigger').click(function (e) {
+//   $(this).createModal({
+//     text: {
+//       title: 'Modal title #2',
+//       body: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Repudiandae mollitia,',
+//     },
+//     btns: {
+//       count: 2,
+//       settings: [
+//         ['Close', ['btn-danger'], true],
+//         ['Submit', ['btn-success', 'ml-20'], false, () => alert('submit')],
+//       ],
+//     },
+//   });
+// });
